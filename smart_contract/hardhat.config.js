d3908d4bd8f6b69f2ab831551f6e76dec425f421
TejasLokesh <55910703+TejasLokesh@users.noreply.github.com>
2023-05-24 12:04:16 -0400

    Used Sepolia Test Network instead of Ropsten Network and deployed smart contract and blockchain functionality

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,9 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/z4WpA8UKgqnwbTYmrZu15yCOiijBKaRv',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/SUhNaCazdHltzuJ4psAkKpXNv103nmEJ',
+      accounts: ['60eb1845a6a061824b98eadb262e2bb5fdc9f6aed9069ea66186abaa44788129'],
     },
   },
 };
